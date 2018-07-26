98dbe0f5e43d90790a2eca9a533184ae2f659cfc
tbxy09 <tb_xy09@163.com>
2018-07-26 11:05:00 +0800

    commit before change

--- util/gen_expand.py
+++ util/gen_expand.py
@@ -26,23 +26,41 @@ def encode_page_features(df):
 
 def min_float(type_):
     return np.finfo(type_).eps
+
 def normalize(rewards):
     return (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
+
 def mplot(env):
     for i in range(6):
         yield pd.Series(env.matrix[i]).plot()
+
 def mmplot(env):
     for i,keys in zip(range(6),['cpu','mem','disk','p','m','pm']):
         print(i,keys)
         yield pd.Series(env.matrix[i][9339:9339+6000]/env.mn[keys]).plot()
+
 def dplot(env):
     for i,keys in zip(range(6),['c','m','d','p_pm','m_pm','pm']):
         print(i,keys)
         yield pd.Series(env.deploy_state[keys]).plot()
+
 def numplot(env):
     pd.Series(env.a_i).plot()
+
 def unit_max(env):
     for key in ['c','m','d','p_pm','m_pm','pm']:
         print(key,(np.argmax(env.unit[key]),env.unit[key].max()))
+
 def app_view(env,aid):
     env.app[env.app.aid==aid].head()
+
+def elegant_stack():
+
+    xn=np.empty_like(np.identity(5)[i])
+    xn_li=[]
+    [xn_li.append(np.identity(5)[i]) for i in range(3)]
+    xn_li.append(xn)
+    # xn=np.sum([xn ,np.sum(xn_li,axis=0)],axis=0)
+    xn=np.sum(xn_li,axis=0)
+
+
